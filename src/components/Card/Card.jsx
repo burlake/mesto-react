01cccf7ed2ab5c29e1d73ab5365492e640a0273c
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-08-03 19:45:10 +0300

    Добавила лайки

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,25 @@
-function Card({ card, onImageCard, onTrashButton }) {
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+
+function Card({ card, onCardLike, onImageCard, onTrashButton}) {
+
+  const currentUser = useContext (CurrentUserContext)
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = ( 
+    `card__like ${isLiked && 'card__like_active'}` 
+  );
+
   return (
     <div className="card">
       <div className="gallery_template">
         <img className="card__img" src={card.link} alt={`${card.name}. Иллюстрация.`} onClick={() => onImageCard({ name: card.name, link: card.link })}/>
         <div className="card__container">
           <h2 className="card__place">{card.name}</h2>
-          <button className="card__like" type="button" />
+          <button className={cardLikeButtonClassName} type="button" onClick={() => onCardLike(card)}/>
           <span className="card__number-of-likes">{card.likes.length}</span>
         </div>
-        <button className="card__trash" type="button" aria-label="Корзина" onClick={onTrashButton}/>
+        {isOwn && <button className="card__trash" type="button" aria-label="Корзина" onClick={onTrashButton}/>}
       </div>
     </div>
   );
