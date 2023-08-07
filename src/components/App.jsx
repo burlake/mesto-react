80afd7e03d36ab77fa2edc8816dcc810fd12f3c1
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-08-07 09:00:07 +0300

    Исправление

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -73,7 +73,15 @@ function App() {
     if (event.target === event.currentTarget) {
       setCloseAllPopups()
     }
-  }, [setCloseAllPopups])
+  }, [
+    isEditProfilePopupOpen, 
+    isEditAvatarPopupOpen, 
+    isAddPlacePopupOpen, 
+    isImageCardChoose, 
+    selectedCard, 
+    isDeletePlacePopupOpen,
+    isDeletePlacePopupOpen
+  ])
 
   const closeAllPopups = useCallback(() => {   // для закрытия по Esc, Overlay и крестику
     setCloseAllPopups()
